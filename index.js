00540ae378c2cd5426f443e05a3ff5414b892448
omri ungar <ungar23@gmail.com>
2022-09-27 17:59:50 +0300

    modal

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-// let coin_data = getcoins();
 const app = document.querySelector('#app');
-moveTo('home');
+let coin_data;
+let more_info = {};
+let report_list = [];
+let inter = function(){};
+console.log(inter);
+
+let test = {1:3,2:4};
+test.college={}
+test.college.life=3
+test[1]['timer']=2
+test[1['trime']]=5
+console.log(test);
+
+(async () => {
+    app.innerHTML = `<div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>`
+    let api = await fetch (`https://api.coingecko.com/api/v3/coins/list`);
+    coin_data = await api.json();
+    moveTo('home');
+ })()
 
 
 function navTo(elem){
@@ -12,12 +31,12 @@ function navTo(elem){
 }
 
 async function moveTo(target) {
-
+    clearInterval(inter);
     let html;
     app.innerHTML = '';
     switch (target) {
-        case 'home': html = await home(); break;
-        case 'reports': html = reports(); break;
+        case 'home': html = home(); break;
+        case 'reports': graph_build(['USD','ILS','EUR']); return;
         case 'about': html = about(); break;
         default: html = home(); break;
     }
@@ -27,32 +46,73 @@ async function moveTo(target) {
 
 }
 
-// function nav(){
-     
-            
-// }
-
-
-async  function home() {
+function home() {
     app.innerHTML = `<div class="spinner-border" role="status">
                         <span class="visually-hidden">Loading...</span>
                     </div>`
-    coin_data = await getcoins();
     app.innerHTML=``
     let card_html ='';
-     for(let i=3; i<=100; i++){
+     for(let i=0; i<=10; i++){
         let coin = coin_data[i];
         card_html += new Card(coin).create();
        }
        return `<div class="boxes container-md">${card_html}</div>`;
-
 }
 
-function reports(){
-    return `<div class="container reports">
-                <h1>Live Reports</h1>
-                <p>This is the live reports page</p>
-            </div>`;
+function reports(my_data){
+    console.log('in');
+        let graph_data = [];
+    Object.entries(my_data).forEach(e => 
+        {let tname = (e[0])
+        let timing = (e[1])
+            graph_data.push({
+                type:"line",
+                axisYType: "secondary",
+                name: tname,
+                showInLegend: true,
+                markerSize: 0,
+                yValueFormatString: "$#.###",
+                dataPoints: timing
+            })        
+        })
+        console.log(graph_data);
+        var chart = new CanvasJS.Chart("app", {
+            title: {
+                text: "House Median Price"
+            },
+            axisX: {
+                // interval: 3,
+                // intervalType: "second"
+                valueFormatString: " HH:mm:ss"
+            },
+            axisY2: {
+                title: "Median List Price",
+                prefix: "$",
+                suffix: ""
+            },
+            toolTip: {
+                shared: true
+            },
+            legend: {
+                cursor: "pointer",
+                verticalAlign: "top",
+                horizontalAlign: "center",
+                dockInsidePlotArea: true,
+                itemclick: toogleDataSeries
+            },
+            data: graph_data
+        }
+        );
+        chart.render();
+        
+        function toogleDataSeries(e){
+            if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                e.dataSeries.visible = false;
+            } else{
+                e.dataSeries.visible = true;
+            }
+            chart.render();
+        }
 }
 
 function about() {
@@ -63,25 +123,32 @@ function about() {
 
 }
 async function moreinfo(a_coin){
+    if(a_coin.getAttribute('aria-expanded')=='false'){
+        return;
+    }
     let the_span = document.getElementById(`s_${a_coin.id}`);
-    let more_info = await getcoins(a_coin.id);
-    setTimeout(() => {
-        the_span.innerHTML = new Info(more_info).create();
-    }, 2000);
+    if(a_coin.id in more_info && ( Date.now() - more_info[`${a_coin.id}`].test_time < 120000)){
+        the_span.innerHTML = new Info(more_info[a_coin.id]).create();
+    }else{
+        the_span.innerHTML = `<div><span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                Loading...</div>`
+        let api = await fetch (`https://api.coingecko.com/api/v3/coins/${a_coin.id}`);
+        more_info[a_coin.id] = await api.json();
+        more_info[`${a_coin.id}`].test_time = new Date().getTime();
+        setTimeout(() => {
+            the_span.innerHTML = new Info(more_info[a_coin.id]).create();
+        }, 2000);
+    }
 }
 
-async function getcoins(id){
-   id = id == undefined ? 'list/' : id; 
-   console.log(id);
-   let api = await fetch (`https://api.coingecko.com/api/v3/coins/${id}`);
-   return await api.json();
-}
 
 
 
-// ===============
-// =====calss=====
-// ===============
+
+
+// ==================================================
+// =======================calss======================
+// ==================================================
 
 class Card{
     constructor(obj){
@@ -99,14 +166,13 @@ class Card{
                         </p>
                         <div class="collapse" id="i_${this.obj.id}">
                         <div class="card card-body">
-                            <span class="dropdown" id="s_${this.obj.id}"><span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                            Loading...</span>
+                            <span class="dropdown" id="s_${this.obj.id}"></span>
                         </div>
                     </div>
 
 
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                    <div class="form-check form-switch main_switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="t_${this.obj.symbol}" onclick="change(this)" ison="false" >
                     </div>
                 </div>
                 
@@ -125,4 +191,109 @@ class Info{
                     ILS:${this.obj.market_data.ath.ils} </br></div>
                     <div class="thumb"><img  src="${this.obj.image.small}"></div>`
     };
+}
+
+class Charting{
+constructor(){
+
+}
+
+create(){
+    
+}
+
+}
+
+function change(coin_switch){
+    coin_switch.getAttribute('ison')=='false' ? toggle_on(coin_switch) : toggle_off(coin_switch);
+    
+}
+
+function toggle_on(coin_switch){
+    if(report_list.length < 5){
+        console.log(23);
+        report_list.push(coin_switch.id.slice(2));
+        coin_switch.setAttribute('ison','true');
+        console.log(report_list);
+    } else{
+        event.preventDefault();
+        popup(report_list,coin_switch);
+    }
+}
+function toggle_off(coin_switch){
+    coin_switch.setAttribute('ison','false')
+    console.log('now off');
+    report_list = (report_list.filter(n => n !== coin_switch.id.slice(2)));
+    console.log(report_list);
+}
+
+async function graph_build(arr){
+    let my_data_point ={};
+    arr.forEach(e => {
+        console.log(e);
+        my_data_point[`${e}`] = [];
+    })
+    let my_url = arr.join();
+    inter = setInterval(async () => {
+        let api = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${my_url}`);
+        let res = await api.json();
+        arr.forEach(e => {
+            let x = new Date();
+            let y = res[`${e}`];
+            let obj = {x:x, y:y};
+            my_data_point[`${e}`].push(obj);
+        })        
+        console.log(my_data_point);
+        reports(my_data_point);
+    }, 2000);
+    inter();
+}
+// graph_build(['USD','ILS','EUR']);
+
+
+function popup(list,coin_switch){
+    popupdiv = document.createElement("div");
+    popupdiv.classList.add('pop');
+    popupdiv.classList.add('modal-dialog');
+    popupdiv.classList.add('modal-lg');
+    let x_pos = (event.clientX);
+    let y_pos = (event.clientY);
+    let html = '';
+    list.forEach(e => {
+        html += `
+                <div class="innerpop">${e}</div>
+                <div class="form-check form-switch innerpop">
+                    <input class="form-check-input" type="checkbox" role="switch" id="k_${e}" onclick="change2(this)" ison="true" checked >
+                </div> `
+    })
+    popupdiv.innerHTML = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Only 5 coins allowed! If you want to add another to the report, choose one to remove</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" onclick="elem_delete()" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ${html}
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" onclick="elem_delete()" data-bs-dismiss="modal">Continue</button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>`;
+    app.appendChild(popupdiv);
+    $('#exampleModal').modal('show');
+    console.log(app);
+}
+
+function change2(coin_switch){
+    let original = `t${coin_switch.id.slice(1)}`
+    console.log(original);
+    $(`#${original}`).click(); 
+}
+
+function elem_delete(){
+    let elem_delete = document.getElementById("exampleModal");
+    elem_delete.remove();
 }
